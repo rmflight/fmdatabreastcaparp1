f90e289949c758da3b502de525e45818c175f66b
Robert Flight <r.flight79@gmail.com>
2014-12-04 10:25:35 -0500

    added the get_chr function to the data package

---
+++ R/fmdatamcf7parp1.R
@@ -0,0 +1,14 @@
+#' get chromosome
+#' 
+#' from a filename, get the chromosome part of the filename
+#' 
+#' @param filename the filename to parse
+#' @param split_chr the character separating the chr* from the rest
+#' @return string of the chromosome
+#' @export
+get_chr <- function(filename, split_chr){
+  file_split <- strsplit(filename, split_chr)[[1]]
+  n_split <- length(file_split)
+  chr_part <- file_split[n_split]
+  substr(chr_part, 1, nchar(chr_part)-4)
+}
