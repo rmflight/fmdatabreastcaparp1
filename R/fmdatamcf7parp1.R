00759bde61f24a9175cd18539426c7addc8c481c
Robert Flight <r.flight79@gmail.com>
2014-12-04 12:05:29 -0500

    added the parp1 reads files for ln4 and ln5, both full and unique

--- R/fmdatamcf7parp1.R
+++ R/fmdatamcf7parp1.R
@@ -12,3 +12,42 @@ get_chr <- function(filename, split_chr){
   chr_part <- file_split[n_split]
   substr(chr_part, 1, nchar(chr_part)-4)
 }
+
+#' All the PARP1 reads from LN4
+#'
+#' A dataset containing all the PARP1 reads from LN4
+#'
+#' @format A GRanges object.
+#'
+#' @source Processing reads from YFM
+"parp1_ln4_reads_all"
+
+#' All the PARP1 reads from LN5
+#'
+#' A dataset containing all the PARP1 reads from LN4
+#'
+#' @format A GRanges object.
+#'
+#' @source Processing reads from YFM
+"parp1_ln5_reads_all"
+
+
+#' The unique location PARP1 reads from LN4
+#'
+#' A dataset containing only the unique location PARP1 reads from LN4, with the number of reads
+#' at each location capped at 6.
+#'
+#' @format A GRanges object with an mcols DataFrame with a column named n_count
+#'
+#' @source Processing reads from YFM
+"parp1_ln4_unique"
+
+#' The unique location PARP1 reads from LN5
+#'
+#' A dataset containing only the unique location PARP1 reads from LN5, with the number of reads
+#' at each location capped at 6.
+#'
+#' @format A GRanges object with an mcols DataFrame with a column named n_count
+#'
+#' @source Processing reads from YFM
+"parp1_ln5_unique"
